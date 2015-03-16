0e839179967d8fc70a8d889a725f13bb1b08f4f6
José Saturnino Pimentel <isc_saturnino@outlook.com>
2015-03-16 01:01:06 -0600

    First Commit

---
+++ DeclarandoDelegados/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DeclarandoDelegados
+{
+	class Program
+	{
+		delegate bool EsPar(int a);
+
+		static void Main(string[] args)
+		{
+			EsPar esPar = MetodoEsPar;
+		}
+
+		private static bool MetodoEsPar(int a)
+		{
+			return a % 2 == 0;
+		}
+
+	}
+}
